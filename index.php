11e6c54235c7c438e1036ad6ca016ad6afcbf411
Geteli <geteli.h@gmail.com>
2024-10-31 14:54:34 +0200

    31.10 static

--- index.php
+++ index.php
@@ -1,31 +1,8 @@
 <?php
-//code author
 
-class Job {
-    public function task($logger){
-        for($i=0;$i<10;$i++) {
-            $logger= new ConsoleLogger();
-            $logger->log("Task $i complete!");
-        }
-
-    }
+class Box {
+    public static $count;
 }
-class ConsoleLogger {
-    public function log($message) {
-        echo $message . "\n";
-    }
-}
-
-
-
-// code user
-class FileLogger {
-    public function log($message){
-        $file = fopen('./log.txt', 'a');
-        fwrite($file, $message. "\n");
-        fclose($file);
-    }
-}
-$job = new Job();
-$logger = new ConsoleLogger();
-$job->task($logger);
+$box1 = new Box();
+$box2 = new Box();
+$box1::$count = 1
